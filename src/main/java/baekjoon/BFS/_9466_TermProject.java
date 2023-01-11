945ed3fd8717e190a8b7bfb2e6a36a39d126f956
gdtknight <gdtknight@naver.com>
2023-01-11 17:13:37 +0900

    [baekjoon] 9466번 진행

--- src/main/java/baekjoon/BFS/_9466_TermProject.java
+++ src/main/java/baekjoon/BFS/_9466_TermProject.java
@@ -16,13 +16,12 @@ public class _9466_TermProject {
   static Queue<Integer> queue = new LinkedList<>();
 
   public static void main(String[] args) throws IOException {
-    // String filePathRoot =
-    // "/home/ubuntu/workspace/coding-test-study/src/main/resources";
-    // String packagePath = "/baekjoon/BFS";
-    // BufferedReader br = new BufferedReader(new InputStreamReader(
-    // new FileInputStream(filePathRoot + packagePath + "/_9466_TestCase")));
+    String filePathRoot = "/home/ubuntu/workspace/coding-test-study/src/main/resources";
+    String packagePath = "/baekjoon/BFS";
+    BufferedReader br = new BufferedReader(new InputStreamReader(
+        new FileInputStream(filePathRoot + packagePath + "/_9466_TestCase")));
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     numsOfCase = Integer.parseInt(br.readLine());
 
@@ -36,47 +35,44 @@ public class _9466_TermProject {
       for (int i = 1; i <= numsOfStudent; i++) {
         choice[i] = Integer.parseInt(st.nextToken());
       }
-      System.out.println(countSolo());
+
+      int cnt = 0;
+
+      boolean[] haveTeam = new boolean[numsOfStudent + 1];
+      boolean[] visited = new boolean[numsOfStudent + 1];
+
+      for (int i = 1; i <= numsOfStudent; i++) {
+        if (haveTeam[i] || visited[i]) {
+          continue;
+        }
+
+        visited[i] = true;
+        queue.offer(i);
+        int next = choice[i];
+
+        while (!visited[next]) {
+          visited[next] = true;
+          queue.offer(next);
+          next = choice[next];
+        }
+
+        while (!queue.isEmpty()) {
+          int cur = queue.poll();
+          if (next == cur) {
+            haveTeam[cur] = true;
+            next = choice[cur];
+          }
+        }
+
+        if (!haveTeam[i]) {
+          cnt += 1;
+        }
+      }
+
+      System.out.println(cnt);
     }
 
     // 결과 출력 부분
     br.close();
-  }
-
-  public static int countSolo() {
-    int cnt = 0;
-
-    boolean[] haveTeam = new boolean[numsOfStudent + 1];
-    boolean[] visited = new boolean[numsOfStudent + 1];
-
-    for (int i = 1; i <= numsOfStudent; i++) {
-      if (haveTeam[i] || visited[i]) {
-        continue;
-      }
-
-      visited[i] = true;
-      queue.offer(i);
-      int next = choice[i];
-
-      while (!visited[next]) {
-        visited[next] = true;
-        queue.offer(next);
-        next = choice[next];
-      }
-
-      while (!queue.isEmpty()) {
-        int cur = queue.poll();
-        if (next == cur) {
-          haveTeam[cur] = true;
-          next = choice[cur];
-        }
-      }
-
-      if (!haveTeam[i]) {
-        cnt += 1;
-      }
-    }
-
-    return cnt;
   }
 }
