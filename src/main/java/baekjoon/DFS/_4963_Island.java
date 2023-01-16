2625a873ee6e7f114466dd971b70949ed66b1549
gdtknight <gdtknight@naver.com>
2023-01-16 09:38:24 +0900

    [baekjoon] 4963번 문제 및 테스트케이스 추가

---
+++ src/main/java/baekjoon/DFS/_4963_Island.java
@@ -0,0 +1,131 @@
+package baekjoon.DFS;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class _4963_Island {
+
+  static int[][] dirs = new int[][] {
+      { 1, 0 },
+      { 0, 1 },
+      { -1, 0 },
+      { 0, -1 },
+      { 1, 1 },
+      { -1, -1 },
+      { 1, -1 },
+      { -1, 1 }
+  };
+
+  static int W = -1;
+  static int H = -1;
+
+  public static void main(String[] args) throws IOException {
+    String filePathRoot = "/home/ubuntu/workspace/coding-test-study/src/main/resources/testcase";
+    String packagePath = "/baekjoon/DFS";
+    BufferedReader br = new BufferedReader(new InputStreamReader(
+        new FileInputStream(filePathRoot + packagePath + "/4963")));
+
+    // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+    W = Integer.parseInt(st.nextToken());
+    H = Integer.parseInt(st.nextToken());
+
+    Stack<Island> stack = new Stack<>();
+
+    while (W != 0 && H != 0) {
+      char[][] map = new char[H][W];
+
+      for (int h = 0; h < H; h++) {
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int w = 0; w < W; w++) {
+          map[h][w] = st.nextToken().charAt(0);
+        }
+      }
+
+      // printMap(map);
+
+      int islandCount = 0;
+
+      for (int h = 0; h < H; h++) {
+        for (int w = 0; w < W; w++) {
+          if (map[h][w] == '1') {
+            islandCount += 1;
+
+            map[h][w] = '2';
+            stack.push(new Island(h, w));
+
+            while (!stack.isEmpty()) {
+              Island cur = stack.pop();
+
+              int curH = cur.getH();
+              int curW = cur.getW();
+
+              for (int i = 0; i < dirs.length; i++) {
+
+                int nextH = curH + dirs[i][0];
+                int nextW = curW + dirs[i][1];
+
+                if (isIn(nextH, nextW) && map[nextH][nextW] == '1') {
+                  map[nextH][nextW] = '2';
+                  stack.push(new Island(nextH, nextW));
+                }
+              }
+            }
+          }
+        }
+      }
+
+      // printMap(map);
+
+      System.out.println(islandCount);
+
+      st = new StringTokenizer(br.readLine(), " ");
+
+      W = Integer.parseInt(st.nextToken());
+      H = Integer.parseInt(st.nextToken());
+    }
+
+    br.close();
+  }
+
+  private static boolean isIn(int nextH, int nextW) {
+    return (0 <= nextH && nextH < H)
+        && (0 <= nextW && nextW < W);
+
+  }
+
+  private static void printMap(char[][] map) {
+    System.out.println();
+    for (int i = 0; i < map.length; i++) {
+      for (int j = 0; j < map[0].length; j++) {
+        System.out.print(map[i][j] + " ");
+      }
+      System.out.println();
+    }
+    System.out.println();
+  }
+}
+
+class Island {
+  int h;
+  int w;
+
+  public Island(int h, int w) {
+    this.h = h;
+    this.w = w;
+  }
+
+  public int getH() {
+    return h;
+  }
+
+  public int getW() {
+    return w;
+  }
+}
