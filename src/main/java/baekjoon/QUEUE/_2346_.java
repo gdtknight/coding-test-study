dfd903438aad4108c06def82fdcf2981197acce7
gdtknight <gdtknight@naver.com>
2023-01-25 11:14:04 +0900

    [baekjoon] 2346번 리플렉션 적용

--- src/main/java/baekjoon/QUEUE/_2346_.java
+++ src/main/java/baekjoon/QUEUE/_2346_.java
@@ -4,16 +4,21 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class _2346_ {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, ClassNotFoundException {
     // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String rootPath = "/home/ubuntu/workspace/coding-test-study/src/main/resources/testcase/";
+    String className = new Object() {
+    }
+        .getClass()
+        .getEnclosingClass()
+        .getName();
+
     BufferedReader br = new BufferedReader(new FileReader(
-        new File("/home/ubuntu/workspace/coding-test-study/src/main/resources/testcase/baekjoon/QUEUE/2346")));
+        new File(rootPath + "/" + className.replaceAll("_", "").replaceAll("[.]", "/"))));
 
     int N = Integer.parseInt(br.readLine());
 
