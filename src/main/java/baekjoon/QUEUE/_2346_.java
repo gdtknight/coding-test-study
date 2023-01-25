6973805715d54e92b039ffb45968b63e234f7fc7
gdtknight <gdtknight@naver.com>
2023-01-25 10:32:05 +0900

    [baekjoon] 2346번 풀이 완료

---
+++ src/main/java/baekjoon/QUEUE/_2346_.java
@@ -0,0 +1,119 @@
+package baekjoon.QUEUE;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class _2346_ {
+  public static void main(String[] args) throws IOException {
+    // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedReader br = new BufferedReader(new FileReader(
+        new File("/home/ubuntu/workspace/coding-test-study/src/main/resources/testcase/baekjoon/QUEUE/2346")));
+
+    int N = Integer.parseInt(br.readLine());
+
+    DoubleLinkedList dl = new DoubleLinkedList(N);
+    int[] next = new int[N + 1];
+
+    for (int i = 1; i <= N; i++) {
+      dl.addLast(i);
+    }
+
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+    int idx = 1;
+    while (st.hasMoreTokens()) {
+      next[idx++] = Integer.parseInt(st.nextToken());
+    }
+
+    while (!dl.isEmpty()) {
+      int cur = dl.removeFirst();
+      System.out.print(cur + " ");
+      int nextStep = next[cur];
+      while (!dl.isEmpty() && nextStep > 1) {
+        dl.addLast(dl.removeFirst());
+        nextStep -= 1;
+      }
+      while (!dl.isEmpty() && nextStep < 0) {
+        dl.addFirst(dl.removeLast());
+        nextStep += 1;
+      }
+    }
+
+    br.close();
+
+  }
+
+}
+
+class DoubleLinkedList {
+  int[] arr;
+  int startIdx;
+  int endIdx;
+
+  DoubleLinkedList(int size) {
+    this.startIdx = 1;
+    this.endIdx = 2;
+    arr = new int[size + 1];
+  }
+
+  public boolean isEmpty() {
+    return startIdx == (endIdx - 1 + arr.length) % arr.length;
+  }
+
+  public boolean isFull() {
+    return endIdx == startIdx;
+  }
+
+  public void addFirst(int n) {
+    if (isFull()) {
+      System.out.println("DoubleLinkedList full");
+      return;
+    }
+
+    arr[startIdx] = n;
+    startIdx = (startIdx - 1 + arr.length) % arr.length;
+    return;
+  }
+
+  public void addLast(int n) {
+    if (isFull()) {
+      System.out.println("DoubleLinkedList is full.");
+      return;
+    }
+
+    arr[endIdx] = n;
+    endIdx = (endIdx + 1) % arr.length;
+
+    return;
+  }
+
+  public int removeFirst() {
+    if (isEmpty()) {
+      System.out.println("DoubleLinkedList is empty.");
+      return -1;
+    }
+
+    startIdx = (startIdx + 1) % arr.length;
+    int n = arr[startIdx];
+
+    return n;
+  }
+
+  public int removeLast() {
+    if (isEmpty()) {
+      System.out.println("DoubleLinkedList is empty.");
+      return -1;
+    }
+
+    endIdx = (endIdx - 1 + arr.length) % arr.length;
+    int n = arr[endIdx];
+
+    return n;
+  }
+}
