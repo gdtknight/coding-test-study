0418ab8849e7efc88698da28b34cc01768de8955
gdtknight <gdtknight@naver.com>
2023-01-26 09:44:24 +0900

    [baekjoon] Main Code Base 추가

---
+++ src/main/java/baekjoon/Main.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+  public static void main(String[] args) throws IOException, ClassNotFoundException {
+    // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    //
+    String rootPath = "/home/ubuntu/workspace/coding-test-study/src/main/resources/testcase/";
+    Object o = new Object() {
+    };
+    String className = o.getClass().getEnclosingClass().getName();
+    String fullPath = rootPath + "/" + className.replaceAll("_", "").replaceAll("[.]", "/");
+    BufferedReader br = new BufferedReader(new FileReader(new File(fullPath)));
+    //
+
+  }
+
+}
