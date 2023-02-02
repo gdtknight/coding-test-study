a95f3fdc9158ca1291846ae5e2691eae7df7abd4
gdtknight <gdtknight@naver.com>
2023-02-02 23:56:56 +0900

    [baekjoon] 문제풀이용 Base File 추가

---
+++ baekjoon/src/main/java/baekjoon/Base.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+
+import baekjoon.common.Initialization;
+
+public class Base implements Problem {
+
+  public void solution(String[] args) throws Exception {
+
+    BufferedReader br = Initialization.getBufferedReaderFromClass(this);
+    // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    // Your code
+
+    br.close();
+  }
+
+}
