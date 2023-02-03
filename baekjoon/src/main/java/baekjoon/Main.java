5b7ecb3151a4af0532e249c3dbb30cf136368969
gdtknight <gdtknight@naver.com>
2023-02-03 10:22:23 +0900

    [baekjoon] 1904번 문제 및 테스트케이스 추가

--- baekjoon/src/main/java/baekjoon/Main.java
+++ baekjoon/src/main/java/baekjoon/Main.java
@@ -4,13 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import baekjoon.common.Problem;
-import baekjoon.dp._1912_;
+import baekjoon.dp._1904_;
 
 public class Main {
 
   public static void main(String[] args) throws Exception {
 
-    Problem problem = new _1912_();
+    Problem problem = new _1904_();
 
     Logger log = LoggerFactory.getLogger(Main.class);
 
---
+++ baekjoon/src/main/java/baekjoon/dp/_1904_.java
@@ -0,0 +1,42 @@
+package baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+import baekjoon.common.Initialization;
+import baekjoon.common.Problem;
+
+public class _1904_ implements Problem {
+
+  public void solution(String[] args) throws Exception {
+
+    BufferedReader br = Initialization.getBufferedReaderFromClass(this);
+    // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    // Your code
+    int n = Integer.parseInt(br.readLine());
+
+    // 길이가 n인 2진 수열
+    // 00, 1 -> 둘로 조합
+    // 15746으로 나눈 나머지
+
+    int[] result = new int[1_000_000];
+    result[0] = 1;
+    result[1] = 2;
+
+    if (n < 3) {
+      System.out.println(result[n - 1]);
+      return;
+    }
+
+    for (int i = 2; i < n; i++) {
+      result[i] = result[i - 1] + result[i - 2];
+    }
+
+    System.out.println(result[n - 1]);
+
+    br.close();
+  }
+
+}
