876444bdaf82e8afe0d99707c67799dff0a3e3d1
gdtknight <gdtknight@naver.com>
2023-02-06 09:45:38 +0900

    [baekjoon] 9375번 진행

--- baekjoon/src/main/java/baekjoon/Main.java
+++ baekjoon/src/main/java/baekjoon/Main.java
@@ -3,14 +3,14 @@ package baekjoon;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import baekjoon.bruteforce._2231_;
+import baekjoon.hash._9375_;
 import common.Problem;
 
 public class Main {
 
   public static void main(String[] args) throws Exception {
 
-    Problem problem = new _2231_();
+    Problem problem = new _9375_();
 
     Logger log = LoggerFactory.getLogger(Main.class);
 
--- baekjoon/src/main/java/baekjoon/hash/_9375_.java
+++ baekjoon/src/main/java/baekjoon/hash/_9375_.java
@@ -1,18 +1,16 @@
 package baekjoon.hash;
 
 import java.io.BufferedReader;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import common.Initialization;
 import common.Problem;
 
 public class _9375_ implements Problem {
+  static int result = 0;
 
   public void solution(String[] args) throws Exception {
 
@@ -21,84 +19,54 @@ public class _9375_ implements Problem {
 
     // Your code
     int numsOfCase = Integer.parseInt(br.readLine());
-    List<Map> list = new ArrayList<>();
 
     for (int i = 0; i < numsOfCase; i++) {
-      Map<String, Set<String>> map = new HashMap<>();
+      Map<String, String> map = new HashMap<>();
+      result = 0;
 
       int numsOfLook = Integer.parseInt(br.readLine());
 
       for (int j = 0; j < numsOfLook; j++) {
-        String[] look = br.readLine().split(" ");
-        Set<String> names = map.getOrDefault(look[1], new HashSet<>());
-        names.add(look[0]);
-
-        map.put(look[1], names);
+        String[] str = br.readLine().split(" ");
+        map.put(str[0], str[1]);
       }
 
-      if (map.keySet().size() == 1) {
-        System.out.println(map.entrySet().iterator().next().getValue().size());
-      } else {
-        int result = 0;
+      String[] names = new String[map.keySet().size()];
+      map.keySet().toArray(names);
 
-        for (Entry<String, Set<String>> entry : map.entrySet()) {
-          result += entry.getValue().size();
-        }
-
-        String[] keyArr = new String[map.keySet().size()];
-        map.keySet().toArray(keyArr);
-
-        for (int totalCnt = 2; totalCnt <= keyArr.length; totalCnt++) {
-          result += getCnt(map, keyArr, totalCnt);
-        }
-        System.out.println(result);
+      for (int totalCnt = 1; totalCnt <= map.keySet().size(); totalCnt++) {
+        boolean[] selected = new boolean[map.size()];
+        permutation(map, names, 0, totalCnt, 0, selected);
       }
-
+      System.out.println(result);
     }
 
     br.close();
   }
 
-  public int getCnt(Map<String, Set<String>> map, String[] keyArr, int totalCnt) {
-    int result = 0;
-    boolean[] selected = new boolean[keyArr.length];
-    List<String[]> list = new ArrayList<>();
-
-    permutation(keyArr, 0, totalCnt, 0, selected, list);
-    for (String[] pair : list) {
-      int sum = 1;
-      for (int i = 0; i < pair.length; i++) {
-        sum *= map.get(pair[i]).size();
-      }
-      result += sum;
-    }
-
-    return result;
-  }
-
   public void permutation(
+      Map<String, String> map,
       String[] keyArr,
       int selectedCnt,
       int totalCnt,
       int nextSearchIdx,
-      boolean[] selected,
-      List<String[]> list) {
+      boolean[] selected) {
 
-    if (totalCnt == selectedCnt) {
-      String[] newPair = new String[totalCnt];
-      int idx = 0;
+    if (selectedCnt == totalCnt) {
+      Set<String> selection = new HashSet<>();
       for (int i = 0; i < keyArr.length; i++) {
-        if (selected[i]) {
-          newPair[idx++] = keyArr[i];
+        if (selected[i] && !selection.add(map.get(keyArr[i]))) {
+          return;
         }
       }
-      list.add(newPair);
+
+      result += 1;
       return;
     }
 
     for (int i = nextSearchIdx; i < keyArr.length; i++) {
       selected[i] = true;
-      permutation(keyArr, selectedCnt + 1, totalCnt, i + 1, selected, list);
+      permutation(map, keyArr, selectedCnt + 1, totalCnt, i + 1, selected);
       selected[i] = false;
     }
 
