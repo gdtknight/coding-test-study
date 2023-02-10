5fed2a2c1b6e829d3157faed606676d4e4617c11
gdtknight <gdtknight@naver.com>
2023-02-10 10:23:58 +0900

    [baekjoon] 2503번 문제 및 테스트케이스 추가

--- baekjoon/src/main/java/baekjoon/Main.java
+++ baekjoon/src/main/java/baekjoon/Main.java
@@ -3,14 +3,14 @@ package baekjoon;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import baekjoon.bruteforce._3085_;
+import baekjoon.bruteforce._2503_;
 import common.Problem;
 
 public class Main {
 
   public static void main(String[] args) throws Exception {
 
-    Problem problem = new _3085_();
+    Problem problem = new _2503_();
 
     Logger log = LoggerFactory.getLogger(Main.class);
 
---
+++ baekjoon/src/main/java/baekjoon/bruteforce/_2503_.java
@@ -0,0 +1,74 @@
+package baekjoon.bruteforce;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import common.Initialization;
+import common.Problem;
+
+public class _2503_ implements Problem {
+
+  public void solution(String[] args) throws Exception {
+
+    BufferedReader br = Initialization.getBufferedReaderFromClass(this);
+    // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    // Your code
+    int N = Integer.parseInt(br.readLine());
+    String[][] scoreInfo = new String[N][3];
+
+    for (int i = 0; i < N; i++) {
+      scoreInfo[i] = br.readLine().split(" ");
+    }
+
+    List<String> nums = new ArrayList<>();
+
+    for (int baek = 1; baek <= 9; baek++) {
+      for (int sip = 1; sip <= 9; sip++) {
+        for (int ill = 1; ill <= 9; ill++) {
+
+          String curNum = (baek * 100 + sip * 10 + ill) + "";
+          boolean found = true;
+
+          for (int i = 0; i < N; i++) {
+            int strikeCnt = 0;
+            int ballCnt = 0;
+
+            Set<Integer> checkedDigit = new HashSet<>();
+            for (int numDigit = 0; numDigit < 3; numDigit++) {
+              for (int infoDigit = 0; infoDigit < 3; infoDigit++) {
+                if (!checkedDigit.contains(infoDigit)
+                    && numDigit == infoDigit
+                    && curNum.charAt(numDigit) == scoreInfo[i][0].charAt(infoDigit)) {
+                  checkedDigit.add(infoDigit);
+                  strikeCnt++;
+                } else if (!checkedDigit.contains(infoDigit)
+                    && curNum.charAt(numDigit) == scoreInfo[i][0].charAt(infoDigit)) {
+                  checkedDigit.add(infoDigit);
+                  ballCnt++;
+                }
+              }
+            }
+
+            if (Integer.parseInt(scoreInfo[i][1]) != strikeCnt || Integer.parseInt(scoreInfo[i][2]) != ballCnt) {
+              found = false;
+              continue;
+            }
+
+          }
+
+          if (found) {
+            nums.add(curNum);
+          }
+        }
+      }
+    }
+    System.out.println(nums.size());
+
+    br.close();
+  }
+
+}
