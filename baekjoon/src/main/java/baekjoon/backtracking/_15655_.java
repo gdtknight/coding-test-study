926fd81edf496264efb1ae34c052760b90cc4199
gdtknight <gdtknight@naver.com>
2023-02-12 21:39:42 +0900

    [baekjoon] 15655번 진행

--- baekjoon/src/main/java/baekjoon/backtracking/_15655_.java
+++ baekjoon/src/main/java/baekjoon/backtracking/_15655_.java
@@ -1,6 +1,8 @@
 package baekjoon.backtracking;
 
 import java.io.BufferedReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 import common.Initialization;
 import common.Problem;
@@ -12,9 +14,45 @@ public class _15655_ implements Problem {
     BufferedReader br = Initialization.getBufferedReaderFromClass(this);
     // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    // Your code
+    // Initialization
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+
+    int[] nums = new int[N];
+
+    st = new StringTokenizer(br.readLine(), " ");
+    for (int i = 0; i < N; i++) {
+      nums[i] = Integer.parseInt(st.nextToken());
+    }
+
+    // Your Code
+    Arrays.sort(nums);
+
+    boolean[] visited = new boolean[N];
+    int[] selection = new int[M];
+
+    permutation(nums, 0, 0, M, visited, selection);
 
     br.close();
   }
 
+  private void permutation(int[] nums, int startIdx, int selectedLength, int targetLength, boolean[] visited,
+      int[] selection) {
+    if (selectedLength == targetLength) {
+      String result = Arrays.toString(selection).replaceAll("[\\[\\]\\,]", "");
+      // String result = Arrays.toString(selection);
+      System.out.println(result);
+      return;
+    }
+
+    for (int i = startIdx; i < nums.length; i++) {
+      visited[i] = true;
+      selection[selectedLength] = nums[i];
+      permutation(nums, i + 1, selectedLength + 1, targetLength, visited, selection);
+      visited[i] = false;
+
+    }
+  }
 }
