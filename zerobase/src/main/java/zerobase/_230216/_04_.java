6a1486e2b4fde238d8d820cef0583e05e16a40cd
gdtknight <gdtknight@naver.com>
2023-02-16 18:48:54 +0900

    [zerobase] 230216 - 4번, 5번 문제 추가

--- zerobase/src/main/java/zerobase/_230216/_04_.java
+++ zerobase/src/main/java/zerobase/_230216/_04_.java
@@ -7,11 +7,35 @@ public class _04_ implements Problem {
     // 2 <= x.length = y.length <= 100000
     // -100000 <= x[i], y[i] <= 100000
     // 0 <= k <= 1000
-    int[] x = new int[] { 1, 2, 5, 6 };
-    int[] y = new int[] { 3, 1, 10, -9 };
-    int k = 2;
 
+    // case1 : 기대값 5
+    // int[] x = new int[] { 1, 2, 5, 6 };
+    // int[] y = new int[] { 3, 1, 10, -9 };
+    // int k = 2;
+
+    // case2 : 기대값 12
+    int[] x = new int[] { 4, 12, 15, 19, 21, 25 };
+    int[] y = new int[] { 8, -4, 7, 1, 4, -9 };
+    int k = 4;
+
+    System.out.println("Test: " + test(x, y, k));
     System.out.println(solution(x, y, k));
+  }
+
+  private int test(int[] x, int[] y, int k) {
+    int maxVal = 0;
+    for (int idxI = 0; idxI < x.length - 1; idxI++) {
+      for (int idxJ = idxI + 1; idxJ < y.length; idxJ++) {
+        if (Math.abs(x[idxI] - x[idxJ]) <= k) {
+          int curVal = y[idxI] + y[idxJ] + Math.abs(x[idxI] - x[idxJ]);
+          if (maxVal < curVal) {
+            maxVal = curVal;
+          }
+
+        }
+      }
+    }
+    return maxVal;
   }
 
   public int solution(int[] x, int[] y, int k) {
--- zerobase/src/main/java/zerobase/_230216/_05_.java
+++ zerobase/src/main/java/zerobase/_230216/_05_.java
@@ -1,29 +1,79 @@
 package zerobase._230216;
 
+import java.util.Arrays;
+
 import common.Problem;
 
 public class _05_ implements Problem {
   public void solution(String[] args) throws Exception {
-    // 0 < n <= 1000
-    int n = 5;
 
-    System.out.println(solution(n));
+    // 0 < N <= 100
+    // 0 < stones.length <= 1000
+    int N = 6;
+    int[][] stones = new int[][] { { 0, 1 }, { 0, 2 }, { 5, 3 }, { 4, 3 } };
+
+    System.out.println(solution(N, stones));
   }
 
-  public int solution(int n) {
+  public int solution(int N, int[][] stones) {
     int answer = 0;
 
-    int[] num = new int[n];
+    boolean[][] board = new boolean[N][N];
+    int[] row = new int[N];
+    int[] col = new int[N];
 
-    int curNum = 1;
+    for (int i = 0; i < stones.length; i++) {
+      board[stones[i][0]][stones[i][1]] = true;
+      row[stones[i][0]] += 1;
+      col[stones[i][1]] += 1;
+    }
 
-    // for (int fiveCnt = 0;; fiveCnt++) {
-    // for (int threeCnt = 0;; threeCnt++) {
-    // for (int twoCnt = 0;; twoCnt++) {
-    // curNum *=
-    // }
-    // }
-    // }
+    while (true) {
+
+      int maxRowIdx = -1;
+      int maxElementsCntOfRow = 0;
+      for (int i = 0; i < N; i++) {
+        if (row[i] <= 0) {
+          continue;
+        }
+
+        if (maxElementsCntOfRow < row[i]) {
+          maxElementsCntOfRow = row[i];
+          maxRowIdx = i;
+        }
+      }
+
+      int maxColIdx = -1;
+      int maxElementsCntOfCol = 0;
+      for (int i = 0; i < N; i++) {
+        if (col[i] <= 0) {
+          continue;
+        }
+
+        if (maxElementsCntOfCol < col[i]) {
+          maxElementsCntOfCol = col[i];
+          maxColIdx = i;
+        }
+      }
+
+      if (maxRowIdx == -1 && maxColIdx == -1) {
+        break;
+      }
+
+      answer += 1;
+
+      if (maxElementsCntOfRow > maxElementsCntOfCol) {
+        row[maxRowIdx] = 0;
+        for (int i = 0; i < N; i++) {
+          col[i] -= 1;
+        }
+      } else {
+        col[maxColIdx] = 0;
+        for (int i = 0; i < N; i++) {
+          row[i] -= 1;
+        }
+      }
+    }
 
     return answer;
   }
