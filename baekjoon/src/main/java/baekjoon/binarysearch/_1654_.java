7545f38a03b1a457be883aaa787d3fa4f10c95bd
gdtknight <gdtknight@naver.com>
2023-02-18 21:54:50 +0900

    [baekjoon] 1654번 진행

--- baekjoon/src/main/java/baekjoon/binarysearch/_1654_.java
+++ baekjoon/src/main/java/baekjoon/binarysearch/_1654_.java
@@ -1,6 +1,7 @@
 package baekjoon.binarysearch;
 
 import java.io.BufferedReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 import common.Initialization;
@@ -15,8 +16,39 @@ public class _1654_ implements Problem {
 
     // Your code
     StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
     int K = Integer.parseInt(st.nextToken());
     int N = Integer.parseInt(st.nextToken());
+
+    int[] lans = new int[K];
+
+    for (int i = 0; i < K; i++) {
+      lans[i] = Integer.parseInt(br.readLine());
+    }
+
+    Arrays.sort(lans);
+
+    int minLen = 0;
+    int maxLen = lans[lans.length - 1];
+    int len = 0;
+    int result = 0;
+
+    while (minLen < maxLen) {
+      len = (minLen + maxLen) / 2;
+
+      result = 0;
+      for (int i = 0; i < K; i++) {
+        result += lans[i] / len;
+      }
+
+      if (result < N) {
+        maxLen = len;
+      } else {
+        minLen = len + 1;
+      }
+    }
+
+    System.out.println(minLen - 1);
 
     br.close();
   }
