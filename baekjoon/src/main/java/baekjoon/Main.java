acc739af64b70f991a258bc7b6584b61e7dc2c2b
gdtknight <gdtknight@naver.com>
2023-03-06 10:28:07 +0900

    [baekjoon] 11053번 문제 및 테스트케이스 추가

--- baekjoon/src/main/java/baekjoon/Main.java
+++ baekjoon/src/main/java/baekjoon/Main.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import baekjoon.dp._9465_;
+import baekjoon.dp._11053_;
 import common.Problem;
 
 public class Main {
@@ -14,7 +14,7 @@ public class Main {
 
     Logger log = LoggerFactory.getLogger(Main.class);
 
-    Problem problem = new _9465_();
+    Problem problem = new _11053_();
 
     String[] pathFrag = problem.getClass().getName().replaceAll("_", "").split("\\.");
 
